0dc19e98ad2ff0556560071730edcfc204d56ce9
LucasCta <lucas.ct.araujo@gmail.com>
2022-11-17 14:34:48 -0300

    Simple Fix

--- 1.5.2/tempo.c
+++ 1.5.2/tempo.c
@@ -5,7 +5,8 @@ int espera = 500;
 int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms){
     if (SDL_WaitEventTimeout(evt, espera)){
     	espera -= (SDL_GetTicks() - *ms);
-    	return 1;
+    	if (espera <= 0) espera = 1;
+		return 1;
     } else {
     	espera = 500;
     	return 0;
@@ -23,12 +24,12 @@ int main (int argc, char* args[]){
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
     /* EXECUÇÃO */
-    SDL_Rect r = { 40,20, 10,10 };
+    SDL_Rect k = { 40,20, 10,10 };
     while (1) {
         SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-        SDL_RenderFillRect(ren, &r);
+        SDL_RenderFillRect(ren, &k);
         SDL_RenderPresent(ren);
 
         SDL_Event evt;
@@ -38,22 +39,32 @@ int main (int argc, char* args[]){
             if (evt.type == SDL_KEYDOWN) {
                 switch (evt.key.keysym.sym) {
                     case SDLK_UP:
-                        r.y -= 5;
+                        k.y -= 5;
+                        if (k.y < 0) k.y = 0;
                         break;
                     case SDLK_DOWN:
-                        r.y += 5;
+                        k.y += 5;
+                        if (k.y > 90) k.y = 90;
                         break;
                     case SDLK_LEFT:
-                        r.x -= 5;
+                        k.x -= 5;
+                        if (k.x < 0) k.x = 0;
                         break;
                     case SDLK_RIGHT:
-                        r.x += 5;
+                        k.x += 5;
+                        if (k.x > 190) k.x = 190;
                         break;
                 }
+            } else if (evt.type == SDL_WINDOWEVENT){
+            	if (SDL_WINDOWEVENT_CLOSE == evt.window.event){
+            		SDL_DestroyRenderer(ren);
+					SDL_DestroyWindow(win);
+					SDL_Quit();
+			    }
             }
         } else {
-            r.x += 2;
-            r.y += 2;
+            if (k.x < 190) k.x += 2;
+            if (k.y < 90) k.y += 2;
         }
     }
 
