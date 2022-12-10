04ba063ae1544f7296ca04f641730d749ce05a7a
LucasCta <lucas.ct.araujo@gmail.com>
2022-12-10 16:27:00 -0300

    Final drag.c

--- 2.x/drag.c
+++ 2.x/drag.c
@@ -1,8 +1,9 @@
 #include <assert.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 #define MAX(x,y) (((x) > (y)) ? (x) : (y))
 #define MIN(x,y) (((x) < (y)) ? (x) : (y))
-
+	
 int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms){
     Uint32 antes = SDL_GetTicks();
     if (SDL_WaitEventTimeout(evt, *ms)) {
@@ -29,13 +30,18 @@ int main (int argc, char* args[]){
 	
 	int clicked = 0;
 	int dragged = 0;
-	int reset = 0;
 	
+    char* texto[] = {"Ready!","Cancelled!","Clicked!","Dropped!","Dragging...","Selected","Unselected"};
+    enum states {ready=0,cancelled,click,dropped,dragging,selected,unselected}; 
+	int cur_state;
+
 	SDL_Rect cube = {75,25,50,50};
 	int oPos[2] = {75,25};
 	
-    int espera = 100;
+    int espera = 30;
+    int contEspera = 0;
 	int running = 1;
+	
 	
     while (running) {
     
@@ -43,6 +49,7 @@ int main (int argc, char* args[]){
         SDL_RenderClear(ren);
 	    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0x00,0x00);
         SDL_RenderFillRect(ren, &cube);
+        stringRGBA(ren, 5, 5, texto[cur_state], 0x00,0x00,0x00,0xFF);
         SDL_RenderPresent(ren);
         
         SDL_Event evt;	
@@ -57,36 +64,47 @@ int main (int argc, char* args[]){
                         cube.x = oPos[0]; cube.y = oPos[1] ;
                         clicked = 0;
                         dragged = 0;
-                        reset = 1;   
+                        cur_state = cancelled;  
                     } break;
                 case SDL_MOUSEBUTTONDOWN:
                     SDL_GetMouseState(&x,&y);
                     oPos[0] = cube.x; oPos[1] = cube.y;
-                    clicked = 1;
+                    clicked = 0;
+                    if (cube.x < x && cube.x + 50 > x && cube.y < y && cube.y + 50 > y)
+                        clicked = 1;
                     dragged = 0;
-				    reset = 0;
 				    break;
 				case SDL_MOUSEBUTTONUP:
-				    if (!dragged && !reset) 
-				        printf("Clicked");
+				    if (!dragged && clicked && cur_state == selected)
+				        cur_state = unselected;
+				    else if (!dragged && clicked) 
+				        cur_state = click;
+				    else if (dragged && clicked)
+				        cur_state = dropped;
 				    clicked = 0;
 				    dragged = 0;
-				    reset = 0;
 				    break;
 			    case SDL_MOUSEMOTION:
 			        if (SDL_GetMouseFocus()){
 			            SDL_GetMouseState(&newX, &newY);
 				        if (clicked){
-				            if (cube.x < x && cube.x + 50 > x && cube.y < y && cube.y + 50 > y){
-	                    		cube.x = MAX(0, MIN(cube.x + (newX - x), 150));
-	                    		cube.y = MAX(0, MIN(cube.y + (newY - y), 50));
-	                    	} dragged = 1;
+				            cur_state = dragging;
+	                    	cube.x = MAX(0, MIN(cube.x + (newX - x), 150));
+	                        cube.y = MAX(0, MIN(cube.y + (newY - y), 50));
+	                        dragged = 1;
 				        } x = newX; y = newY; 
-				    } else clicked = 0;
-				    break;
+				    } else {
+				        clicked = 0;
+				        if (dragged) cur_state = dropped;
+				        dragged = 0;
+				    } break;
             }
         } else {
-            espera = 100; 
+            espera = 30; 
+            if (clicked && !dragged){
+                if (contEspera++ == 10)
+                    cur_state = selected;
+            } else contEspera = 0;
         }   
 	   
     }
