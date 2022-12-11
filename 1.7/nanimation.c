3b4233c67676b38044dceb65b87b707e9371e99d
LucasCta <lucas.ct.araujo@gmail.com>
2022-12-11 15:37:41 -0300

    1.7 final cycle animation

--- 1.7/nanimation.c
+++ 1.7/nanimation.c
@@ -15,24 +15,7 @@ int AUX_WaitEventTimeoutCount(SDL_Event* evt, Uint32* ms){
     } else return 0;
 }
 
-void desenhaBoneco(int v[100][2], SDL_Renderer* ren){
-	SDL_SetRenderDrawColor(ren, 0x20,0x20,0x50,0x00); 
-	SDL_RenderClear(ren);
-	int c1 = filledCircleColor(ren, 50, 20, 10, 0xFFBBBBBB);
-	int c2 = filledCircleColor(ren, 50, 40, 20, 0xFFBBBBBB);
-	int c3 = filledCircleColor(ren, 50, 60, 30, 0xFFBBBBBB);
-	int b1 = filledCircleColor(ren, 50, 45, 2, 0xFF000000);
-	int b2 = filledCircleColor(ren, 50, 60, 2, 0xFF000000);
-	int b3 = filledCircleColor(ren, 50, 75, 2, 0xFF000000);
-	int o1 = filledCircleColor(ren, 45, 20, 1, 0xFF000000);
-	int o2 = filledCircleColor(ren, 55, 20, 1, 0xFF000000);
-	Sint16 v1[] = {50,58,50}; Sint16 v2[] = {24,26,28};
-	int cen = filledPolygonRGBA(ren, v1, v2, 3, 0xFF, 0x57, 0x33, 255);
-	SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-	/*int i = 0; for (i; i<100; i++){
-		SDL_RenderDrawPoint(ren, v[i][0],v[i][1]);
-	} */ SDL_RenderPresent(ren); 
-}
+//void desenhaBoneco(int v[100][2], SDL_Renderer* ren){}
 
 int main (int argc, char* args[]){
 
@@ -58,14 +41,22 @@ int main (int argc, char* args[]){
 	
 	int running = 1;
 	int espera = 400;
+	int cont = 0;
+	int cut = 1;
 	
     while (running) {
     
     	/* RENDER */
-    	//desenhaBoneco(v, ren);
+	    SDL_SetRenderDrawColor(ren, 0x20,0x20,0x50,0x00); 
+	    SDL_RenderClear(ren);
         SDL_Rect snowman = (SDL_Rect){1,1,100,100};
-		SDL_RenderCopy(ren, img, &snowman, &snowman);
-		SDL_RenderPresent(ren);
+        SDL_Rect snowmanStage = (SDL_Rect){cut,1,100,100};
+		SDL_RenderCopy(ren, img, &snowmanStage, &snowman);
+	    SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+    	int i = 0; 
+    	for (i; i<100; i++)
+		    SDL_RenderDrawPoint(ren, v[i][0],v[i][1]);
+	    SDL_RenderPresent(ren); 
 		
 		/* EVENTOS */
 		SDL_Event evt;
@@ -77,12 +68,28 @@ int main (int argc, char* args[]){
             	}
             }
         } else {
-			int j = 0; 
+			int j = 0;
 			for (j; j<100; j++){
 			   v[j][0] += 1; v[j][1] += 3;
 			   if (v[j][0] >= 100) v[j][0] -= 100;
 			   if (v[j][1] >= 100) v[j][1] -= 100;	
-			} espera = 400; 
+			}
+			if (cont == 450) {
+			    cut = 1;
+			    cont = 0;
+                espera = 400; 
+			} else if (cont >= 390) {
+			    cut += 100;
+			    cont += 1;
+                espera = 10; 
+			} else if (cont >= 40) { 
+			    cont += 10;
+			    espera = 400 - cont;
+			} else {
+                espera = 400; 
+			    if (cont++%10 == 9)
+			        cut += 100; 
+		    }
 		}
 		
 	}
@@ -94,33 +101,3 @@ int main (int argc, char* args[]){
 	SDL_Quit();
 
 }
-
-/*
-	else{   
-	
-		switch(isup){
-			case 1:
-				c = (SDL_Rect) { 0, 64, 30,32 };
-			break;
-			case 2:
-				c = (SDL_Rect) { 125,64, 30,32 };
-			break;
-			case 3:
-				c = (SDL_Rect) { 158,64, 30,32 };
-			break;
-			case 4:
-				c = (SDL_Rect) { 125,64, 30,32 };
-			break;
-		} isup++;
-	
-		
-        SDL_Rect r = { x,50, 100,80 };
-        SDL_Rect c;
-        
-		SDL_RenderPresent(ren);
-		espera = 250;
-		x = (x + 10) % 400;
-		if(isup > 4){
-			isup = 1;
-		}
-*/
