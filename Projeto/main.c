a91120a0f656b0b1e39729213d49902acb213623
LucasCta <lucas.ct.araujo@gmail.com>
2022-12-31 20:47:48 -0300

    trying to implement tff

--- Projeto/main.c
+++ Projeto/main.c
@@ -1,5 +1,6 @@
 #include <assert.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #define MAX(x,y) (((x) > (y)) ? (x) : (y))
 #define MIN(x,y) (((x) < (y)) ? (x) : (y))
 
@@ -24,42 +25,80 @@ int main (int argc, char* args[]){
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
     /* EXECUÇÃO */
+    
+    enum tela {menu=0,telaInicial,fim};
+    enum states {idle=0,walking,pushing,talking,interacting};
+    
     SDL_Rect player = {40,20,100,100};
+    int pState = idle;
     int pSpeed = 5;
-    int running = 1;
-    int espera = 20;
+    
+    int screen = menu;
+    int espera = 100;
+    
+    TTF_Init();
+    TTF_Font *font = TTF_OpenFont("zk.ttf",100);
     
     /* EVT LOOP */
-    while (running) {
-        SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
-        SDL_RenderClear(ren);
-        SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
-        SDL_RenderFillRect(ren, &player);
-        SDL_RenderPresent(ren);
-        SDL_Event evt;
-        int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
-        if (isevt){
-            if (evt.type == SDL_KEYDOWN){
-                switch (evt.key.keysym.sym){
-                     case SDLK_UP:
-                        player.y = MAX(player.y - pSpeed, 0);
-                        break;
-                    case SDLK_DOWN:
-                        player.y = MIN(player.y + pSpeed, h - player.h);
-                        break;
-                    case SDLK_LEFT:
-                        player.x = MAX(player.x - pSpeed, 0);
-                        break;
-                    case SDLK_RIGHT:
-                        player.x = MIN(player.x + pSpeed, w - player.w);
-                        break;
-                }
-            } else if (evt.type == SDL_WINDOWEVENT){
-            	if (SDL_WINDOWEVENT_CLOSE == evt.window.event){
-					running = 0;
-            	}
-            } 
-        } else espera = 20;
+    while (screen < fim) {
+    
+        while (screen == menu){
+        
+            SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+            SDL_RenderClear(ren);
+        
+            SDL_Rect recPlay = {500,500,200,200};
+            SDL_Color grey = {125,125,125,125};
+            struct SDL_Surface * play = TTF_RenderText_Solid(font,"Play",grey);    
+            struct SDL_Texture * playBut = SDL_CreateTextureFromSurface(ren,play);
+            SDL_RenderCopy(ren,playBut,NULL,&recPlay);
+	
+		    SDL_RenderPresent(ren);
+		    
+		    SDL_Event evt;
+            int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
+            if (isevt){
+                if (evt.type == SDL_WINDOWEVENT){
+                	if (SDL_WINDOWEVENT_CLOSE == evt.window.event)
+                	    screen = fim;
+                } 
+            } else espera = 100;
+            
+        }
+        
+        while (screen == telaInicial){
+            SDL_Rect npc = {400,400,100,100};
+            SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
+            SDL_RenderClear(ren);
+            SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
+            SDL_RenderFillRect(ren, &player);
+            SDL_RenderPresent(ren);
+            SDL_Event evt;
+            int isevt = AUX_WaitEventTimeoutCount(&evt, &espera);
+            if (isevt){
+                if (evt.type == SDL_KEYDOWN){
+                    switch (evt.key.keysym.sym){
+                         case SDLK_UP:
+                            player.y = MAX(player.y - pSpeed, 0);
+                            break;
+                        case SDLK_DOWN:
+                            player.y = MIN(player.y + pSpeed, h - player.h);
+                            break;
+                        case SDLK_LEFT:
+                            player.x = MAX(player.x - pSpeed, 0);
+                            break;
+                        case SDLK_RIGHT:
+                            player.x = MIN(player.x + pSpeed, w - player.w);
+                            break;
+                    }
+                } else if (evt.type == SDL_WINDOWEVENT){
+                	if (SDL_WINDOWEVENT_CLOSE == evt.window.event){
+					    screen = fim;
+                	}
+                } 
+            } else espera = 20;
+        }
+        
     }
 
 	/* FINALIZACAO */
